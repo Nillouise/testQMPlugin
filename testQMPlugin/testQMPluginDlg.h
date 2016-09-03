2d0f24f4968f0b53975a2e0ec065dcf621d1979e
Nillouise <LovelyNihilism@outlook.com>
2016-09-03 12:36:51 +0800

    Add project files.

---
+++ testQMPlugin/testQMPluginDlg.h
@@ -0,0 +1,36 @@
+
+// testQMPluginDlg.h : header file
+//
+
+#pragma once
+
+
+// CtestQMPluginDlg dialog
+class CtestQMPluginDlg : public CDialogEx
+{
+// Construction
+public:
+	CtestQMPluginDlg(CWnd* pParent = NULL);	// standard constructor
+
+// Dialog Data
+#ifdef AFX_DESIGN_TIME
+	enum { IDD = IDD_TESTQMPLUGIN_DIALOG };
+#endif
+
+	protected:
+	virtual void DoDataExchange(CDataExchange* pDX);	// DDX/DDV support
+
+
+// Implementation
+protected:
+	HICON m_hIcon;
+
+	// Generated message map functions
+	virtual BOOL OnInitDialog();
+	afx_msg void OnSysCommand(UINT nID, LPARAM lParam);
+	afx_msg void OnPaint();
+	afx_msg HCURSOR OnQueryDragIcon();
+	DECLARE_MESSAGE_MAP()
+public:
+	afx_msg void OnBnClickedOk();
+};
